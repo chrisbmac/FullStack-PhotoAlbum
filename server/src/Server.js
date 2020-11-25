d4179d91aef7faf23c92ed90a5e64d5d5b632a6c
Chris MacFarlane <chris-mac.06@hotmail.com>
2020-11-25 16:30:07 -0400

    finished add comment, working on sorting comments

--- server/src/Server.js
+++ server/src/Server.js
@@ -21,6 +21,7 @@ app.use(sanitizer());
 // express static middleware - setup static files location
 app.use("/", express.static('./build'));
 
+// ----------------------------------------- get
 app.get("/get", async (request, response) => {
     // construct MongoClient object for working with MongoDB
     let mongoClient = new MongoClient(URL, { useUnifiedTopology: true });
@@ -28,7 +29,7 @@ app.get("/get", async (request, response) => {
     try {
         await mongoClient.connect(); 
         // convert all documents in technologies collection into array in one awesome statement!
-        let techArray = await mongoClient.db(DB_NAME).collection("photos").find().sort("title",1).toArray();
+        let techArray = await mongoClient.db(DB_NAME).collection("photos").find().sort("comments", -1).toArray();
         let json = { "photos": techArray };
         
         response.status(200);
@@ -43,7 +44,8 @@ app.get("/get", async (request, response) => {
     }
 });
 
-/*app.post("/post", async (request, response) => {
+// ----------------------------------------- post add
+app.post("/post", async (request, response) => {
     // construct MongoClient object for working with MongoDB
     let mongoClient = new MongoClient(URL, { useUnifiedTopology: true });
     // Use connect method to connect to the server
@@ -59,16 +61,16 @@ app.get("/get", async (request, response) => {
         // console.log(request.body.description);
 
         // sanitize all the input
-        request.body.name = request.sanitize(request.body.name);
-        request.body.description = request.sanitize(request.body.description);
-        request.body.difficulty = request.sanitize(request.body.difficulty);
-        request.body.courses.forEach(course => {
-            course.code = request.sanitize(course.code);
-            course.name = request.sanitize(course.name);
+        request.body.title = request.sanitize(request.body.title);
+        request.body.caption = request.sanitize(request.body.caption);
+        request.body.source = request.sanitize(request.body.source);
+        request.body.comments.forEach(comments => {
+            comments.comment = request.sanitize(comments.comment);
+            comments.author = request.sanitize(comments.author);
         });
 
         // insert a new document into the database
-        let result = await mongoClient.db(DB_NAME).collection("technologies").insertOne(request.body);
+        let result = await mongoClient.db(DB_NAME).collection("photos").insertOne(request.body);
 
         // response with result JSON for client side to use
         response.status(200);
@@ -82,8 +84,41 @@ app.get("/get", async (request, response) => {
         mongoClient.close();
     }
 });
+app.post("/addComment/:id", async (request, response) => {
+    let mongoClient = new MongoClient(URL, { useUnifiedTopology: true });
+    try {
+        await mongoClient.connect(); 
+        let id = new ObjectId(request.sanitize(request.params.id));
 
+        /*request.body.title = request.sanitize(request.body.tile);
+        request.body.description = request.sanitize(request.body.description);
+        request.body.difficulty = request.sanitize(request.body.difficulty);*/
+        request.body.comments.forEach(comment => {
+            comment.author = request.sanitize(comment.author);
+            comment.comment = request.sanitize(comment.comment);
+        });
+        let selector = { "_id":id };
+        let newValues = { $push: {"comments": request.body.comments } };
 
+        let result = await mongoClient.db(DB_NAME).collection("photos").updateOne(selector, newValues);
+
+        if (JSON.parse(result).n <= 0) {
+            response.status(404);
+            response.send({error: "No technology documents found with ID"});
+            mongoClient.close();
+            return;
+        }
+        response.status(200);
+        response.send(result);     
+    } catch (error) {
+        response.status(500);
+        response.send({error: error.message});
+        throw error;
+    } finally {
+        mongoClient.close();
+    }
+});
+// ----------------------------------------- put update
 app.put("/put/:id", async (request, response) => {
     // construct MongoClient object for working with MongoDB
     let mongoClient = new MongoClient(URL, { useUnifiedTopology: true });
@@ -94,7 +129,7 @@ app.put("/put/:id", async (request, response) => {
         // sanitize all the input
         let id = new ObjectId(request.sanitize(request.params.id));
 
-        request.body.name = request.sanitize(request.body.name);
+        request.body.title = request.sanitize(request.body.tile);
         request.body.description = request.sanitize(request.body.description);
         request.body.difficulty = request.sanitize(request.body.difficulty);
         request.body.courses.forEach(course => {
@@ -128,6 +163,31 @@ app.put("/put/:id", async (request, response) => {
         // close mongoClient (connection to MongoDB server)
         mongoClient.close();
     }
-});*/
+});
+
+// ----------------------------------------- Delete
+app.delete("/delete/:id", async (request, response) => {
+    let mongoClient = new MongoClient(URL, { useUnifiedTopology: true });
+    try {
+        await mongoClient.connect(); 
+        let id = new ObjectId(request.sanitize(request.params.id));
+        let result = await mongoClient.db(DB_NAME).collection("technologies").deleteOne({_id:id});
+        console.log(id);
+        if (JSON.parse(result).n <= 0) {
+            response.status(404);
+            response.send({error: "No technology documents found with ID " + id});
+            mongoClient.close();
+            return;
+        }
+        response.status(200);
+        response.send(result);     
+    } catch (error) {
+        response.status(500);
+        response.send({error: error.message});
+        throw error;
+    } finally {
+        mongoClient.close();
+    }
+});
 
 app.listen(8080, () => console.log("Listening on port 8080"));
